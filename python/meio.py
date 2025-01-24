e0d9ab2030c2fcd92c3f6ac935755b2bf986134b
jorgequiteja <jorge.viana@quiteja.com.br>
2025-01-24 18:32:45 -0300

    Orientação a objetos

--- python/meio.py
+++ python/meio.py
@@ -1,6 +1,8 @@
 # aqui estou vendo como funcionam os módulos e importações
 # operações assíncronas e talvez orientação a objetos
 
+# ------------ Módulos e importações --------------
+
 from inicio import funcao
 # então quando importa o código de inicio é executado :O
 # que loucura
@@ -15,6 +17,9 @@ import asyncio # essa é padrão do python
 print(aiohttp.__file__) # /home/jorgeviana/anaconda3/lib/python3.11/site-packages/aiohttp/__init__.py
 print(asyncio.__file__) # /home/jorgeviana/anaconda3/lib/python3.11/asyncio/__init__.py
 
+
+# ------------ Operações assíncronas --------------
+
 async def fetch_data():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://api.sampleapis.com/futurama/info') as response:
@@ -22,3 +27,12 @@ async def fetch_data():
             print(data)
 
 asyncio.run(fetch_data())
+
+
+# ------------ Orientação a objetos --------------
+
+class Animal:
+    def __init__(self, nome):
+        self.nome =  nome
+    def fazer_som():
+        print()
