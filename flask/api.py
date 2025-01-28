fe7612c6d13f5cdaa69e5fb25597344b56c6042a
jorgequiteja <jorge.viana@quiteja.com.br>
2025-01-28 08:22:23 -0300

    Usando template

--- flask/api.py
+++ flask/api.py
@@ -1,13 +1,17 @@
 # hora de aprender Flask
-from flask import Flask
+from flask import Flask, render_template
 from pages import home_view
 #Criando uma instancia de flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return home_view
+    return render_template('index.html', nome="visitante")
 
 if (__name__) == '__main__':
     # faz reload quando muda alguma coisa na aplicação
     app.run(debug=True)
+
+@app.route('/post', methods=['POST'])
+def post_example():
+    return "Você fez uma requisição POST!"
