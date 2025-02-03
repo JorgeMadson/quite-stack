7601d43552e08fd196a517d5b87d44f7fb66b5b1
jorgequiteja <jorge.viana@quiteja.com.br>
2025-02-03 11:33:28 -0300

    Adicionando o flower e organizando projeto

--- 4-projeto/api.py
+++ 4-projeto/api.py
@@ -6,12 +6,14 @@ print('Iniciando o servidor')
 app = Flask(__name__)
 
 #Flask
-#aqui vou fazer o projeto com tudo funcionando
+@app.route("/")
+def health():
+    return("API de pé, tudo certinho 👌")
+
 @app.route("/email", methods=['POST'])
 def receber_requisicao():
     json_do_post = request.get_json()
-    while range(0, 1_000_000):
-        resposta = envio_pra_fila.delay(json_do_post)
+    resposta = envio_pra_fila.delay(json_do_post)
     return resposta.id
 
 if __name__ == '__main__':
--- 4-projeto/tasks.py
+++ 4-projeto/tasks.py
@@ -1,9 +1,14 @@
 from celery import Celery
-import time
-app_celery = Celery('tasks', broker='amqp://guest@localhost//')
+app_celery = Celery('tasks', broker='amqp://guest@localhost//', broker_transport_options={"queue_name_prefix": "email-webhook-"})
+# app_celery = Celery('tasks', broker='amqp://guest@localhost//')
 
 #Celery
-#pra rodar tem que mandar o celery -A tasks worker -l INFO
+# -A ou --app é de aplication
+# pra rodar tem que mandar o celery -A tasks worker -l INFO
+#
+# Adicionei o flower (visualizador de celery) pra dar uma olhada
+# ele sobe na porta 5555, pra rodar:
+# celery -A tasks flower -l INFO
 @app_celery.task
 def envio_pra_fila(data):
     #aqui eu salvo no banco
